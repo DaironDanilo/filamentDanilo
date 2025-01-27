75b5a62c832ef9dadb9abf3406a38196c14496ba
danilo <ddanilodorado@gmail.com>
2025-01-26 19:23:44 -0500

    create slider to control focal length
    - overlay slider composable to control the camera focal length

--- app/src/main/java/com/danilo/filamentdanilo/MainActivity.kt
+++ app/src/main/java/com/danilo/filamentdanilo/MainActivity.kt
@@ -11,15 +11,30 @@ import android.view.WindowManager
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.google.android.filament.Fence
 import com.google.android.filament.IndirectLight
@@ -100,11 +115,60 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    fun HomeScreen(){
+    fun HomeScreen() {
         val title by titleState
-        Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Text(title, color = Color.Black)
+        Box(modifier = Modifier.fillMaxSize()) {
             FilamentSurfaceComposeView()
+            Column(Modifier.align(Alignment.TopCenter)) {
+                Spacer(modifier = Modifier.height(16.dp))
+                if (title.isNotEmpty()) {
+                    ElevatedCard(
+                        elevation = CardDefaults.cardElevation(
+                            defaultElevation = 6.dp
+                        ),
+                        modifier = Modifier
+                            .height(48.dp)
+                    ) {
+                        Text(
+                            title,
+                            color = Color.Black,
+                            fontSize = 24.sp,
+                            modifier = Modifier.padding(8.dp)
+                        )
+                    }
+                }
+            }
+            var sliderPosition by remember { mutableFloatStateOf(0f) }
+            modelViewer.cameraFocalLength = sliderPosition
+            updateRootTransform()
+            Column(modifier = Modifier.align(Alignment.BottomCenter)) {
+                ElevatedCard(
+                    elevation = CardDefaults.cardElevation(
+                        defaultElevation = 6.dp
+                    ),
+                    modifier = Modifier
+                ) {
+                    Column(
+                        Modifier
+                            .fillMaxWidth(0.9f)
+                            .padding(16.dp)
+                    ) {
+                        Slider(
+                            value = sliderPosition,
+                            onValueChange = { sliderPosition = it },
+                            colors = SliderDefaults.colors(
+                                thumbColor = MaterialTheme.colorScheme.secondary,
+                                activeTrackColor = MaterialTheme.colorScheme.secondary,
+                                inactiveTrackColor = MaterialTheme.colorScheme.secondaryContainer,
+                            ),
+                            steps = 40,
+                            valueRange = 50f..90f
+                        )
+                        Text(text = "Camera Focal Length: $sliderPosition", fontSize = 24.sp)
+                    }
+                }
+                Spacer(modifier = Modifier.height(24.dp))
+            }
         }
     }
 
@@ -124,7 +188,7 @@ class MainActivity : ComponentActivity() {
         )
     }
 
-    private fun initSurfaceView(surfaceView: SurfaceView){
+    private fun initSurfaceView(surfaceView: SurfaceView) {
 
         doubleTapDetector = GestureDetector(applicationContext, doubleTapListener)
         singleTapDetector = GestureDetector(applicationContext, singleTapListener)
