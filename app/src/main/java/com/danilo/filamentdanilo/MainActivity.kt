e5e17cd1bec656c620656f386b1e3471084177dc
danilo <ddanilodorado@gmail.com>
2025-02-05 07:55:17 -0500

    delete unused code in main activity
    - delete old function to set the text status in main activity
    - refactor code to place at the top of the main activity the onCreate method and at the bottom the companion object

--- app/src/main/java/com/danilo/filamentdanilo/MainActivity.kt
+++ app/src/main/java/com/danilo/filamentdanilo/MainActivity.kt
@@ -2,32 +2,14 @@ package com.danilo.filamentdanilo
 
 import android.os.Bundle
 import android.view.WindowManager
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.MutableState
 import com.danilo.filamentdanilo.presentation.screens.home.HomeScreen
 import com.google.android.filament.utils.Utils
 
 class MainActivity : ComponentActivity() {
-
-    companion object {
-        // Load the library for the utility layer, which in turn loads gltfio and the Filament core.
-        init {
-            Utils.init()
-        }
-
-        const val TAG = "gltf-viewer"
-    }
-
-    private val onBackPressedCallback: OnBackPressedCallback =
-        object : OnBackPressedCallback(true) {
-            override fun handleOnBackPressed() {
-                finish()
-            }
-        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,18 +21,19 @@ class MainActivity : ComponentActivity() {
         onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
     }
 
-    // TODO move this function to a common place
-    private fun setStatusText(
-        text: String,
-        statusToast: MutableState<Toast?>,
-        statusText: MutableState<String?>,
-    ) {
-        runOnUiThread {
-            if (statusToast.value == null || statusText.value != text) {
-                statusText.value = text
-                statusToast.value = Toast.makeText(applicationContext, text, Toast.LENGTH_SHORT)
-                statusToast.value?.show()
+    private val onBackPressedCallback: OnBackPressedCallback =
+        object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                finish()
             }
         }
+
+    companion object {
+        // Load the library for the utility layer, which in turn loads gltfio and the Filament core.
+        init {
+            Utils.init()
+        }
+
+        const val TAG = "gltf-viewer"
     }
 }
